f90285f0e5cb5445ac7eb38ba834041427680fdd
Jorge Miranda <jorge.miranda@majesticmedia.ca>
2022-04-19 13:27:29 -0600

    new content

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
 
 <h3>Another title</h3>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint qui officiis, saepe autem ratione delectus! Molestiae fugiat quia obcaecati dolor similique? Earum, inventore ullam quaerat minima fuga eaque hic enim.</p>
+
+<h3>Title 3</h3>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut ipsa incidunt, perferendis alias dolores, unde et repellat id quod rerum commodi excepturi quos quidem quibusdam culpa nihil non sit tenetur.</p>
 <!-- content -->  
 
 <?php get_footer(); ?>
